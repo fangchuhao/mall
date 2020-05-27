07dd787a106b6bf466718107f521168307fd465e
fangchuhao <2415966685@qq.com>
2020-05-27 15:41:46 +0800

    'FastClick/Vue-LazyLoad/postcss-px-to-viewport'

--- src/components/content/goods/GoodsItem.vue
+++ src/components/content/goods/GoodsItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods-item" @click="toGoodsDetail">
-    <img :src="goodsItem.image" alt="" @load="imageLoad">
+    <img v-lazy="goodsItem.image" alt="" @load="imageLoad">
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
       <span class="price">{{goodsItem.price}}</span>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store";
 import toast from "./components/common/toast"
+import FastClick from 'fastclick'
+import VueLazyload from "vue-lazyload";
 
 Vue.config.productionTip = false
 
 Vue.prototype.$bus=new Vue()
 
 Vue.use(toast)
+FastClick.attach(document.body)
+Vue.use(VueLazyload,{
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   router,
